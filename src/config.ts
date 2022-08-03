8e6f4c07c084d67f9336173022cdb0d443773501
kullnam11 <kullnam11@gmail.com>
2022-08-03 23:32:13 +0700

    first commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+import { CHAIN_MAP } from './utils/networks';
+
+export const MORALIST_SERVER_URL = 'https://dh8zwhue8abl.usemoralis.com:2053/server';
+export const MORALIS_APP_ID = 'Ei0AU4GNhunMgvgoFm5nCSwh8hAgU3LI98s4lwhH';
+export const MORALIS_RPC_URL = 'https://speedy-nodes-nyc.moralis.io/2c7ea5f467e5a804dec81df4/bsc/testnet';
+export const DEFAULT_CHAIN_NAME: 'rinkeby' | 'bsc testnet' = 'bsc testnet';
+export const DEFAULT_CHAIN_ID = CHAIN_MAP[DEFAULT_CHAIN_NAME];
